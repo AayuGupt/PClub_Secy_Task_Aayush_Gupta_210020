8cd3aa1a5e6c42fafe918bc20ebb636d41bda44f
AayuGupt <100212842+AayuGupt@users.noreply.github.com>
2022-08-16 16:32:23 +0530

    Update Code_to_generate_Test_Cases.cpp

--- Code_to_generate_Test_Cases.cpp
+++ Code_to_generate_Test_Cases.cpp
@@ -1,27 +1,9 @@
 #include<bits/stdc++.h>
-#include <vector>
-#include <math.h>
 #include<time.h>
 using std::ofstream;
 using namespace std;
-#define vi vector<int>
-#define pb push_back
-#define p push
-#define sll stack<long long>
-#define qll queue<long long>
-#define vll vector<long long>
-#define st string
-#define vvll vector<vll>
-#define vstr vector <string>
-#define fori(n) for(ll i=0;i<n;i++)
-#define forj(n) for(int j=0;j<n;j++)
-#define fork(n) for(int k=0;k<n;k++)
-#define fori1(n) for(int i=1;i<n;i++)
-#define f(i,a,b) for(int i=a;i<=b;i++)s
-#define s(v) sort(v.begin(),v.end())
-#define ll long long
-#define mp   make_pair 
 
+#define ll long long
 
 int main(){
     
@@ -29,7 +11,7 @@ int main(){
     ofstream outdata;
     srand(time(NULL));
 
-    outdata.open("chess.dat");
+    outdata.open("test_cases.dat");
     // We can set the number of inputs we need
     for(int tt=0;tt<1000;tt++){
         int n, m;
